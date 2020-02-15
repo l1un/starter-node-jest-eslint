b9b126d056de47ba842c19a8fa50849e763902a5
11un <derekh@tuta.io>
2020-02-15 13:20:56 -0800

    Delete .stylelintrc (unused). Log on npm start.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const lL = require('./src/utils/lLog');
 
-lL('B', 'Hello World');
+console.log(lL('g', 'Hello World'));
+console.log(lL('y', 'Hello World'));
+console.log(lL('r', 'Hello World'));
+console.log(lL('b', 'Hello World'));
+console.log(lL('c', 'Hello World'));
+console.log(lL('m', 'Hello World'));
+console.log(lL('bl', 'Hello World'));
+console.log(lL('re', 'Hello World'));
